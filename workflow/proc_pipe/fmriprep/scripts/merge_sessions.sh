44504e4150b2de3cef26900f5115e14594d54819
nikhil153 <nikhil153@gmail.com>
2023-02-01 21:33:20 -0500

    minor fixes to merge_sessions.sh

--- workflow/proc_pipe/fmriprep/scripts/merge_sessions.sh
+++ workflow/proc_pipe/fmriprep/scripts/merge_sessions.sh
@@ -12,9 +12,9 @@ SUB_DIR=$3
 echo "This will copy (not mv) separately processed session (SES-B) next to (SES-A) in subject dir within $SES_A_DIR"
 
 if [ ! -d "${SES_A_DIR}/${SUB_DIR}" ]; then
-   echo "ses-2 does not exist for ${SUB_DIR}"
+   echo "ses-B does not exist for ${SUB_DIR}"
    mkdir ${SES_A_DIR}/${SUB_DIR}
 fi
 
-rsync -av ${SES_B_DIR}/${SUB_DIR}/ses-* ${SES_A_DIR}/${SUB_DIR}/
-rsync -av ${SES_B_DIR}/${SUB_DIR}/figures/* ${SES_A_DIR}/${SUB_DIR}/figures/
+rsync -a ${SES_B_DIR}/${SUB_DIR}/ses-* ${SES_A_DIR}/${SUB_DIR}/
+rsync -a ${SES_B_DIR}/${SUB_DIR}/figures/* ${SES_A_DIR}/${SUB_DIR}/figures/
